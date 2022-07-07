b1f562672c83db67824834371bcdb38bd037e77d
jonathanw16 <107503242+jonathanw16@users.noreply.github.com>
2022-07-07 13:37:50 -0700

    Add files via upload

---
+++ funcs/checkName.groovy
@@ -0,0 +1,14 @@
+def call(name) {
+    def cluster = Cluster.connect("couchbases://cb.eh32avgkwwptcnks.cloud.couchbase.com", "jonwilcb", "Pixelj_2112")
+    def bucket = cluster.bucket("travel-sample")
+    def scope = bucket.scope("tenant_agent_00")
+    def collection = scope.collection("users")
+
+    try{
+        def docName = collection.get(name)
+        return true 
+    }catch(DocumentNotFoundException docex) {
+        collection.upsert("AMI", name)
+        return false
+    }
+}
---
+++ funcs/updateAmi.groovy
@@ -0,0 +1,18 @@
+def call(name, key, value) {
+    def cluster = Cluster.connect("couchbases://cb.eh32avgkwwptcnks.cloud.couchbase.com", "jonwilcb", "Pixelj_2112")
+    def bucket = cluster.bucket("travel-sample")
+    def scope = bucket.scope("tenant_agent_00")
+    def collection = scope.collection("users")
+
+    try{
+        def docAMI = collection.get(name)
+        def content = docAMI.contentAsObject()
+        content.put(key, value, replaceOptions().cas(docAMI.cas()))
+        collection.replace(name, content, replaceOptions().cas(docAMI.cas()))
+        
+        return 'success'
+    }catch(CouchbaseException cbex) {
+        return 'fail'
+    }
+    
+}

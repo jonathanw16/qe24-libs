0a28131a83b3b8ef9ca3de1d9125f557f4a5f185
Jon <jonathan.wilcox@couchbase.com>
2022-07-12 12:54:34 -0700

    fix

--- vars/checkName.groovy
+++ vars/checkName.groovy
@@ -4,13 +4,17 @@ import com.couchbase.client.core.error.CouchbaseException;
 import com.couchbase.client.core.error.DocumentExistsException;
 import com.couchbase.client.core.error.DocumentNotFoundException;
 
+import com.couchbase.client.java.AsyncCollection;
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.Collection;
+import com.couchbase.client.java.ReactiveCollection;
 import com.couchbase.client.java.Scope;
 import com.couchbase.client.java.json.JsonObject;
 import com.couchbase.client.java.kv.GetResult;
 import com.couchbase.client.java.kv.MutationResult;
+import com.couchbase.client.java.kv.PersistTo;
+import com.couchbase.client.java.kv.ReplicateTo;
 
 def call(name) {
     def cluster = Cluster.connect("couchbases://cb.eh32avgkwwptcnks.cloud.couchbase.com", "jonwilcb", "Pixelj_2112")
@@ -21,7 +25,7 @@ def call(name) {
     try{
         def docName = collection.get(name)
         return true 
-    }catch(DocumentExistsException docex) {
+    }catch(docex) {
         def upsertResult = collection.upsert(name, JsonObject.create().put("AMI", name).put("PIPELINE_STATUS", "STARTED"))
         return false
     }

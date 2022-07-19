a2a4ce195c850239c030805a62bdf6a3f3f10891
jonathanw16 <107503242+jonathanw16@users.noreply.github.com>
2022-07-19 11:25:14 -0700

    Create pipeline.groovy

---
+++ pipeline.groovy
@@ -0,0 +1,39 @@
+@Library("qe24-libs")
+
+package com.couchbase.client
+
+pipeline {
+    agent any
+    parameters {
+        string(name: 'USERNAME', defaultValue: 'jwcb', description: 'username')
+        string(name: 'PASSWORD', defaultValue: 'Pixelj_2112', description: 'use this password')
+        string(name: 'CONNECTSTR', defaultValue: 'couchbases://cb.eh32avgkwwptcnks.cloud.couchbase.com', description: 'connectionstring')
+    }
+    stages {
+        stage('Hello') {
+            steps {
+                echo 'Hello World'
+            }
+        }
+        stage('Get AMI name') {
+            steps {
+                echo 'Starting'
+                script {
+                    if(checkName("JENKINS_TEST6.DEV_PIPELINE.TESTING_STAt", params.CONNECTSTR, params.USERNAME, params.PASSWORD) == true) {
+                        println('success')
+                    } else {
+                        println('fail')
+                    }
+                }
+            }
+        }
+        stage('Update AMI') {
+            steps {
+                echo 'Starting'
+                script {
+                   println(updateAmi("JENKINS_TEST6", "DEV_PIPELINE.TESTING_STAt", "TEST", params.CONNECTSTR, params.USERNAME, params.PASSWORD))
+                }
+            }
+        }
+    }
+}

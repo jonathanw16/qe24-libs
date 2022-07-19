ccf97ef26cc32b6eb73df1b291b0cde9f926192e
jonathanw16 <107503242+jonathanw16@users.noreply.github.com>
2022-07-19 12:42:50 -0700

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -9,7 +9,7 @@ pipeline {
         stage('Hello') {
             steps {
                 script{
-                    @Library("sharedLib")_
+                    library "sharedLib"
                     echo 'Hello World'
                 }
             }

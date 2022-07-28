329b71c1600a56284fe065939bb56ee0b3281028
jonathanw16 <107503242+jonathanw16@users.noreply.github.com>
2022-07-28 11:03:47 -0700

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -19,7 +19,8 @@ pipeline {
             steps {
                 echo 'Starting'
                 script {
-                    sh 'ls'
+                    def lines = script: 'dir', returnStdout: true
+                    echo lines
                     sh 'python3 main.py -u ${params.USERNAME} -p ${params.PASSWORD} -c ${params.CONNECTSTR} -m PYTEST'
                 }
             }

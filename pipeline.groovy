79e783ef9233600ed04f9e40316deb47a0905790
jonathanw16 <107503242+jonathanw16@users.noreply.github.com>
2022-07-28 12:23:26 -0700

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -19,7 +19,7 @@ pipeline {
             steps {
                 echo 'Starting'
                 script {
-                    newDoc = bat (@script: "python3 main.py -u ${params.USERNAME} -p ${params.PASSWORD} -c ${params.CONNECTSTR} -m PYTEST3", returnStdout: true)
+                    newDoc = bat (script: "python3 main.py -u ${params.USERNAME} -p ${params.PASSWORD} -c ${params.CONNECTSTR} -m PYTEST4-${params.USERNAME}", returnStdout: true)
                     echo newDoc
                 }
             }

6185ad5a635ba6bb3e62e88997e87ea75c12eba5
Jon <jonathan.wilcox@couchbase.com>
2022-08-04 11:17:55 -0700

    a

--- pipeline.groovy
+++ pipeline.groovy
@@ -8,18 +8,12 @@ pipeline {
         string(name: 'CONNECTSTR', defaultValue: 'couchbases://cb.jtsutglfipogkncn.cloud.couchbase.com', description: 'connectionstring')
     }
     stages {
-        stage('Hello') {
-            steps {
-                script{
-                    echo 'Hello World'
-                }
-            }
-        }
+        
         stage('Get AMI name') {
             steps {
                 echo 'Starting'
                 script {
-                    newDoc = bat (script: "python3 main.py -u ${params.USERNAME} -p ${params.PASSWORD} -c ${params.CONNECTSTR} -m PYTEST4-${params.USERNAME}", returnStdout: true)
+                    newDoc = bat (script: "python3 main.py -u ${params.USERNAME} -p ${params.PASSWORD} -c ${params.CONNECTSTR} -m amitest", returnStdout: true)
                     echo newDoc
                 }
             }
@@ -28,35 +22,12 @@ pipeline {
             steps {
                 echo 'Starting'
                 script {
-                    bat "python3 main.py -u ${params.USERNAME} -p ${params.PASSWORD} -c ${params.CONNECTSTR} -m PYTEST3 -a update -k test -v test"
-                }
-            }
-        }
-        stage('TESTS') {
-            steps {
-                script {
-                    updateAmi("TEST_DOC", "dev-pipeline.test33", "test", params.CONNECTSTR, params.USERNAME, "${params.PASSWORD}")
-                }
-            }
-        }
-        stage('tests2') {
-            steps {
-                script {
-                    updateAmi("TEST_DOC", "dev-pipeline.test22", "test", params.CONNECTSTR, params.USERNAME, "${params.PASSWORD}")
+                    bat "python3 main.py -u ${params.USERNAME} -p ${params.PASSWORD} -c ${params.CONNECTSTR} -m amitest -a update -k dev.test -v test"
                 }
             }
         }
         
     }
-    post {
-            success {
-                updateAmi("TEST_DOC", "dev-pipeline.TEST10", "SUCCESS", params.CONNECTSTR, params.USERNAME, "${params.PASSWORD}")
-
-            }
-            failure {
-                updateAmi("TEST_DOC", "dev-pipeline.TEST61", "FAIL", params.CONNECTSTR, params.USERNAME, "${params.PASSWORD}")
-
-            }
-        }
+    
 }
 
